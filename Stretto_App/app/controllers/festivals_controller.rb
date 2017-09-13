8b523777303f401bc1c09f8c8c9d96033430f2d2
Rajiste Bennin <rajisteb@gmail.com>
2017-09-13 18:10:51 -0400

    http json response and index view

--- Stretto_App/app/controllers/festivals_controller.rb
+++ Stretto_App/app/controllers/festivals_controller.rb
@@ -1,7 +1,15 @@
+require 'Httparty'
+
 class FestivalsController <ApplicationController
+    include HTTParty
 
     def home
     
+    end
+
+    def index
+        @seatgeek = Rails.application.secrets.seat_geek_api_key
+        @response = HTTParty.get("https://api.seatgeek.com/2/events?q=music_festival&client_id=#{@seatgeek}") 
     end
 
 end
