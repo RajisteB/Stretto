b892521179deaa7e975b85733ad07aec8e150886
Rajiste Bennin <rajisteb@gmail.com>
2017-09-13 18:16:41 -0400

    return json response

--- Stretto_App/app/controllers/festivals_controller.rb
+++ Stretto_App/app/controllers/festivals_controller.rb
@@ -10,6 +10,7 @@ class FestivalsController <ApplicationController
     def index
         @seatgeek = Rails.application.secrets.seat_geek_api_key
         @response = HTTParty.get("https://api.seatgeek.com/2/events?q=music_festival&client_id=#{@seatgeek}") 
+        render json: {data: @response}
     end
 
 end
