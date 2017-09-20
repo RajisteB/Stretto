9f5ad174b31df3cfa88907aa1188e321c3225267
Rajiste Bennin <rajisteb@gmail.com>
2017-09-20 09:35:16 -0400

    finalized

--- Stretto_App/app/controllers/application_controller.rb
+++ Stretto_App/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if user_signed_in?
       user = current_user.id
-      profile_path(user)
+      festivals_path
     else
       new_user_session_path
   end
